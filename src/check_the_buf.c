6b6ef770178fe97b776db1e76f826d8adbe9580e
Ruslan NAUMENKO <rnaumenk@e3r7p8.unit.ua>
2018-03-14 16:07:11 +0200

    test5

--- src/check_the_buf.c
+++ src/check_the_buf.c
@@ -20,7 +20,7 @@ size_t		check_the_buf(t_p *p, size_t len)
 	{
 		temp = p->buf;
 		while ((p->ret + len) > p->buf_size)
-			p->buf_size += 100;
+			p->buf_size += 1;
 		if (!(p->buf = (char*)ft_memalloc(sizeof(char) * (p->buf_size + 1))))
 			p->error = 1;
 		ft_memcpy(p->buf, temp, p->ret);
