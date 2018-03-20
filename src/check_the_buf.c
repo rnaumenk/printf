23bf98c10dc149b3d31d346ae9de339d1899e219
Ruslan NAUMENKO <rnaumenk@e3r5p10.unit.ua>
2018-03-20 16:43:13 +0200

    TIRED

--- src/check_the_buf.c
+++ src/check_the_buf.c
@@ -18,7 +18,7 @@ size_t		check_the_buf(t_p *p, size_t len)
 	{
 		p->tmp = p->buf;
 		while ((p->ret + len) > p->buf_size)
-			p->buf_size += 1;
+			p->buf_size += 50;
 		if (!(p->buf = (char*)ft_memalloc(sizeof(char) * (p->buf_size + 1))))
 			p->error = 1;
 		if (!p->error && &p->buf != &p->tmp)
